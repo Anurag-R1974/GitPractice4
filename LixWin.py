f99bb182e1b249c8cb95de707d3f967e20970bcb
Anurag-R1974 <sanuragramagiri@gmail.com>
2025-04-12 23:31:56 +0530

    modified

--- LixWin.py
+++ LixWin.py
@@ -5,7 +5,7 @@ client = boto3.client('ec2', region_name='us-east-1')  # You can change region a
 response = client.run_instances(
     ImageId='ami-00a929b66ed6e0de6',
     InstanceType='t2.medium',
-    InstanceType='t2.large', # changed from micro to medium in Github
+    InstanceType='t2.MEDIUM', # changed from micro to medium in Github
     KeyName='USKEY1',         # Changed from medium to large in Branch 
     MinCount=1,
     MaxCount=1
