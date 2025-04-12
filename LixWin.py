c3119260f2bb898098910c01d9c7f2184700b283
Anurag-R1974 <sanuragramagiri@gmail.com>
2025-04-12 22:43:00 +0530

    Update LixWin.py micro to medium {Branching}
    Changed from micro to medium to practice in Branching topic

--- LixWin.py
+++ LixWin.py
@@ -4,7 +4,7 @@ client = boto3.client('ec2', region_name='us-east-1')  # You can change region a
 
 response = client.run_instances(
     ImageId='ami-00a929b66ed6e0de6',
-    InstanceType='t2.micro',
+    InstanceType='t2.medium',
     KeyName='USKEY1',
     MinCount=1,
     MaxCount=1
